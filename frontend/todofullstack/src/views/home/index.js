a02b45ea8a4ad1added92cefda378bd9108f5115
rafael <rafaelmdeveloper@gmail.com>
2022-06-26 19:07:53 -0300

    40. Creating the Header with Styled Components

--- frontend/todofullstack/src/views/home/index.js
+++ frontend/todofullstack/src/views/home/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import * as S from './styles';
 
+
+//NOSSOS COMPONENTES
+import Header from '../../components/Header'
 function Home() {
-  return <h1>Vocês está HOME!</h1> 
+  return (
+      <S.Container>
+        <Header/>
+      </S.Container>
+  ) 
 }
 
 export default Home;
