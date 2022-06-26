df2fdaa7dffd98c1abcb8c700f67ade1c448c146
rafael <rafaelmdeveloper@gmail.com>
2022-06-26 16:50:31 -0300

    29. Removing a task

--- backend/src/routes/TaskRoutes.js
+++ backend/src/routes/TaskRoutes.js
@@ -8,6 +8,7 @@ const MacaddressValidation = require('../middlewares/MacaddressValidation');
 router.post('/', TaskValidation, TaskController.create);
 router.put('/:id', TaskValidation, TaskController.update);
 router.get('/:id', TaskController.show);
+router.delete('/:id', TaskController.delete);
 
 router.get('/filter/all', MacaddressValidation, TaskController.all);
 
