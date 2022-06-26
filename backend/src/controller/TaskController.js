d5f795a246fa1f82bbfedc7fd693ccf96910990d
rafael <rafaelmdeveloper@gmail.com>
2022-06-26 17:16:07 -0300

    32. Task of the day filter

--- backend/src/controller/TaskController.js
+++ backend/src/controller/TaskController.js
@@ -1,4 +1,5 @@
 const TaskModel = require('../model/TaskModel');
+const { startOfDay, endOfDay } = require('date-fns');
 
 const current = new Date();
 
@@ -78,6 +79,21 @@ class TaskController {
                 'when': {'$lt': current},
                 'macaddress': {'$in': req.body.macaddress}
         })
+            .sort('when')
+            .then(response => {
+                return res.status(200).json(response);
+            })
+            .catch(error => {
+                return res.status(500).json(error);
+            });
+    }
+
+    async today(req, res){
+        await TaskModel
+            .find({
+                'macaddress': {'$in': req.body.macaddress},
+                'when': {'$gte': startOfDay(current), '$lte': endOfDay(current)}
+            })
             .sort('when')
             .then(response => {
                 return res.status(200).json(response);
