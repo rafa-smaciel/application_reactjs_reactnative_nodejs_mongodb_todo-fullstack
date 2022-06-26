8df32a291c9b68574ce37ec39157536499686cc2
rafael <rafaelmdeveloper@gmail.com>
2022-06-26 17:20:56 -0300

    33. Task of the week filter

--- backend/src/controller/TaskController.js
+++ backend/src/controller/TaskController.js
@@ -1,5 +1,5 @@
 const TaskModel = require('../model/TaskModel');
-const { startOfDay, endOfDay } = require('date-fns');
+const { startOfDay, endOfDay, startOfWeek, endOfWeek } = require('date-fns');
 
 const current = new Date();
 
@@ -93,6 +93,21 @@ class TaskController {
             .find({
                 'macaddress': {'$in': req.body.macaddress},
                 'when': {'$gte': startOfDay(current), '$lte': endOfDay(current)}
+            })
+            .sort('when')
+            .then(response => {
+                return res.status(200).json(response);
+            })
+            .catch(error => {
+                return res.status(500).json(error);
+            });
+    }
+
+    async week(req, res){
+        await TaskModel
+            .find({
+                'macaddress': {'$in': req.body.macaddress},
+                'when': {'$gte': startOfWeek(current), '$lte': endOfWeek(current)}
             })
             .sort('when')
             .then(response => {
