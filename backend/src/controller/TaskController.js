0696b06a7cfda211c5e98ac437cdafc1c63bb0fc
rafael <rafaelmdeveloper@gmail.com>
2022-06-26 17:26:50 -0300

    35. Year task filter

--- backend/src/controller/TaskController.js
+++ backend/src/controller/TaskController.js
@@ -5,7 +5,9 @@ const {
     startOfWeek, 
     endOfWeek,
     startOfMonth,
-    endOfMonth
+    endOfMonth,
+    startOfYear,
+    endOfYear,
 } = require('date-fns');
 
 const current = new Date();
@@ -130,6 +132,21 @@ class TaskController {
             .find({
                 'macaddress': {'$in': req.body.macaddress},
                 'when': {'$gte': startOfMonth(current), '$lte': endOfMonth(current)}
+            })
+            .sort('when')
+            .then(response => {
+                return res.status(200).json(response);
+            })
+            .catch(error => {
+                return res.status(500).json(error);
+            });
+    }
+
+    async year(req, res){
+        await TaskModel
+            .find({
+                'macaddress': {'$in': req.body.macaddress},
+                'when': {'$gte': startOfYear(current), '$lte': endOfYear(current)}
             })
             .sort('when')
             .then(response => {
