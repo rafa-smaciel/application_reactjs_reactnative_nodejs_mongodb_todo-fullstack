7578826d811a4d90d568d90b6d07de80e6c7604a
rafael <rafaelmdeveloper@gmail.com>
2022-07-14 19:50:21 -0300

    50. Registering a task - error

--- frontend/todofullstack/src/views/Task/index.js
+++ frontend/todofullstack/src/views/Task/index.js
@@ -1,3 +1,5 @@
+/* eslint-disable eqeqeq */
+/* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from 'react';
 import * as S from './styles';
 import api from '../../services/api';
@@ -13,13 +15,33 @@ import iconClock from '../../assets/clock.png';
 function Task() {
     const[lateCount, setLateCount] = useState();
     const[type, setType] = useState();
+    const[id, setId] = useState();
+    const[done, setDone] = useState(false);
+    const[title, setTitle] = useState();
+    const[description, setDescription] = useState();
+    const[date, setDate] = useState();
+    const[hour, setHour] = useState();
+    const[macaddress, setMacaddress] = useState();
 
     async function lateVerify(){
     await api.get(`/task/filter/late/11:11:11:11:11:11`)
         .then(response => {
             setLateCount(response.data.length)
      })
-}
+    }
+
+    async function Save(){
+        await api.post('/task', {
+            macaddress,
+            type,
+            title,
+            description,
+            when: `${date}T${hour}:00.000`
+        }).then(() =>
+            alert('TAREFA CADASTRADA COM SUCESSO!')
+        )
+    }
+
   useEffect(() => {
     lateVerify();
   }, [])
@@ -35,7 +57,6 @@ function Task() {
                         index > 0 && 
                         <button type="button" onClick={() => setType(index)}>
                         <img src={icon} alt="Tipo da Tarefa" 
-                        // eslint-disable-next-line eqeqeq
                         className={type && type != index && 'inative'}/>
                         </button>
                         ))
@@ -44,36 +65,40 @@ function Task() {
 
             <S.Input>
                 <span>Título</span>
-                <input type="text" placeholder="Título da Tarefa..."></input>
+                <input type="text" placeholder="Título da Tarefa..." 
+                onChange={e => setTitle(e.target.value)} value={title}/>
             </S.Input> 
 
             <S.TextArea>
-                <span>Título</span>
-                <textarea rows={5} placeholder="Detalhes da Tarefa"></textarea>
+                <span>Descrição</span>
+                <textarea rows={5} placeholder="Detalhes da Tarefa..."
+                onChange={e => setDescription(e.target.value)} value={description}/>
             </S.TextArea> 
 
             <S.Input>
                 <span>Data</span>
-                <input type="date" placeholder="Detalhes da Tarefa"></input>
+                <input type="date" placeholder="Data"
+                onChange={e => setDate(e.target.value)} value={date}/>
                 <img src={iconCalendar} alt="Calendário"/>
             </S.Input> 
 
             <S.Input>
                 <span>Hora</span>
-                <input type="time" placeholder="Detalhes da Tarefa"></input>
-                <img src={iconClock} alt="Calendário"/>
+                <input type="time" placeholder="Hora"
+                onChange={e => setHour(e.target.value)} value={hour}/>
+                <img src={iconClock} alt="Relógio"/>
             </S.Input> 
 
             <S.Options>
                 <div>
-                    <input type="checkbox"/>
+                    <input type="checkbox" checked={done} onChange={() => setDone(!done)}/>
                     <span>CONCLUÍDO</span>
                 </div>
                 <button type="button">EXCLUIR</button>
             </S.Options>
            
             <S.Save>
-                <button type="button">SALVAR</button>
+                <button type="button" onClick={Save}>SALVAR</button>
             </S.Save>
 
         </S.Form>
