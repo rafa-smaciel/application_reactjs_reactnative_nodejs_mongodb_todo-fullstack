0f6b6146feb6821fe9b35de6d495cedcfabd9c63
rafael <rafaelmdeveloper@gmail.com>
2022-07-14 21:50:07 -0300

    BackEnd Checked

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,9 +4,10 @@ const server = express();
 server.use(cors());
 server.use(express.json());
 
+
 const TaskRoutes = require('./routes/TaskRoutes');
 server.use('/task', TaskRoutes);
 
 server.listen(3333, () => {
-    console.log('API ONLINE');
+  console.log('API ONLINE');
 });
--- backend/src/routes/TaskRoutes.js
+++ backend/src/routes/TaskRoutes.js
@@ -4,17 +4,19 @@ const router = express.Router();
 const TaskController = require('../controller/TaskController');
 const TaskValidation = require('../middlewares/TaskValidation');
 
+
 router.post('/', TaskValidation, TaskController.create);
-router.put('/:id', TaskValidation, TaskController.update);
+router.put('/:id', TaskValidation,  TaskController.update);
 router.get('/:id', TaskController.show);
 router.delete('/:id', TaskController.delete);
 router.put('/:id/:done', TaskController.done);
 
-router.get('/filter/all/:macaddress', TaskController.all);
-router.get('/filter/late/:macaddress', TaskController.late);
+router.get('/filter/all/:macaddress',   TaskController.all);
+router.get('/filter/late/:macaddress',  TaskController.late);
 router.get('/filter/today/:macaddress', TaskController.today);
-router.get('/filter/week/:macaddress', TaskController.week);
+router.get('/filter/week/:macaddress',  TaskController.week);
 router.get('/filter/month/:macaddress', TaskController.month);
-router.get('/filter/year/:macaddress', TaskController.year);
+router.get('/filter/year/:macaddress',  TaskController.year);
+
 
 module.exports = router;
