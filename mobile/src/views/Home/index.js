13f138c32ce59d61dbe1755624a68e9872d0c7b2
rafael <rafaelmdeveloper@gmail.com>
2022-07-17 18:20:53 -0300

    67. Creating the Footer component dynamically

--- mobile/src/views/Home/index.js
+++ mobile/src/views/Home/index.js
@@ -5,11 +5,13 @@ import styles from './styles';
 
 // COMPONENTES
 import Header from '../../components/Header';
+import Footer from '../../components/Footer';
 
 export default function Home(){
     return ( 
     <View style={styles.container}>
         <Header showNotification={true}/>
+        <Footer icon={'add'} />
     </View>
     )
 }
