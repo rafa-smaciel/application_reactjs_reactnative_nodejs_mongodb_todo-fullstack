c29fe3adbf2d9abc7525b68e94c89757f4f71e66
rafael <rafaelmdeveloper@gmail.com>
2022-07-22 20:19:04 -0300

    68. Filter Create

--- mobile/src/views/Home/styles.js
+++ mobile/src/views/Home/styles.js
@@ -6,7 +6,25 @@ const styles = StyleSheet.create({
         backgroundColor: '#FFF',
         alignItems: 'center',
         justifyContent: 'flex-start',
-    }   
+    },
+    filter: {
+        flexDirection: 'row',
+        width: '100%',
+        justifyContent: 'space-around',
+        height: 50,
+        alignItems: 'center',
+    },
+    filterTextActived: {
+        fontWeight: 'bold',
+        fontSize: 18,   
+        color: '#EE6B26',
+    },
+    filterTextInative:{
+        color: '#20295f',
+        fontSize: 18,
+        fontWeight: 'bold',
+        opacity: 0.5,
+    },
 });
 
 export default styles;
